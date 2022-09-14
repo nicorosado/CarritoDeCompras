5c5fdc08a403540d381e00f1bd13d23aa3131561
nicorosado <nicorosado.nr@gmail.com>
2022-09-14 16:25:33 -0300

    primer comentario

---
+++ stock.js
@@ -0,0 +1,35 @@
+const productos = [
+    {
+        id:1,
+        nombre:"Alimento Selecta",
+        desc:"alimento para perros",
+        precio:5000,
+        img:'src/img/alimento.jpg',
+        cantidad:1
+    },
+    {
+        id:2,
+        nombre:"Correa",
+        desc:"correa para perro",
+        precio:1500,
+        img:'src/img/correa.jpg',
+        cantidad:1
+    },
+    {
+        id:3,
+        nombre:"Juguete",
+        desc:"juguete para perro",
+        precio:500,
+        img:'src/img/juguete.jpg',
+        cantidad:1
+    },
+    {
+        id:4,
+        nombre:"Pipeta",
+        desc:"pipeta para perro",
+        precio:2000,
+        img:'src/img/pipeta.jpg',
+        cantidad:1
+    },
+]
+
